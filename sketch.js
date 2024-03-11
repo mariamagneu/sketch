acada28807c12acf76fb78431f86c17c341c8fd1
maria magdalena <neumariamagdalena@gmail.com>
2024-03-11 18:05:14 +0100

    append rows divs to body

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const gridContainer = document.querySelector("#myGrid");
-    const numColumns = 16; //
-   
+    const gridContainer = document.querySelector("#myGrid")
+    const rows = document.createElement("div");
+    document.body.appendChild(rows);
+    const numColumns = 16; 
+
+
     
     for (let i = 1; i <= numColumns; i++) {
         const column = document.createElement("div");
-        column.classList.add("column");
-        column.textContent = "test"
-        gridContainer.appendChild(column);
+        column.classList.add("rowContainer");
+        column.textContent = "this is a row";
+        rows.appendChild(column);
     }
     
-
 });
