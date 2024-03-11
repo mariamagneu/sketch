2b86f088f3459b6de46608f6653e658a65349d98
maria magdalena <neumariamagdalena@gmail.com>
2024-03-11 17:42:52 +0100

    add textcontent to .column divs to see them

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gridContainer = document.querySelector("#myGrid");
     const numColumns = 16; //
+   
     
     for (let i = 1; i <= numColumns; i++) {
         const column = document.createElement("div");
         column.classList.add("column");
+        column.textContent = "test"
         gridContainer.appendChild(column);
     }
-
+    
 
 });
