2802547210d14257113186f1862b8a9d6569f356
maria magdalena <neumariamagdalena@gmail.com>
2024-03-12 22:15:11 +0100

    change classname to cell

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const gridContainer = document.querySelector("#myGrid")
-    const rows = document.createElement("div");
-    document.body.appendChild(rows);
+    const myGrid = document.querySelector("#myGrid")
+    const cell = document.createElement("div");
+    document.body.appendChild(cell);
     const numColumns = 16; 
+    const numCells = 16;
 
 
     
     for (let i = 1; i <= numColumns; i++) {
-        const column = document.createElement("div");
-        column.classList.add("rowContainer");
-        column.textContent = "this is a row";
-        rows.appendChild(column);
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.textContent = i;
+        myGrid.appendChild(cell);
     }
+
+    
     
 });
