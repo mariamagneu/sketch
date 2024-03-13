234f1c1002f47f7f08fbf6058a611c94a4d1a5cb
maria magdalena <neumariamagdalena@gmail.com>
2024-03-13 21:41:13 +0100

    make loop start at 0 not at 1

--- sketch.js
+++ sketch.js
@@ -1,25 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const myGrid = document.querySelector("#myGrid")
-    const column = document.createElement("div");
-    document.body.appendChild(column);
     const numColumns = 16; 
     const numRows = 16;
 
 
     
-    for (let i = 1; i <= numColumns; i++) {
+    for (let i = 0; i < numColumns; i++) {
         const column = document.createElement("div");
         column.classList.add("column");
-        column.textContent = i;
         myGrid.appendChild(column);
-       		for (let j = 1; j< numRows; j++) {
+       		for (let j = 0; j< numRows; j++) {
        		const cell = document.createElement("div");
        		cell.classList.add("cell");
-        	cell.textContent = j + '';
-        	column.appendChild(cell);
+					cell.textContent = j + '/' + i;
+					column.appendChild(cell);
     	}    
     }
     
-    
-    
+     
 });
