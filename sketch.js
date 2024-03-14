7a1805ed852913d6717ec6b1c0b5cfb0b67f470c
maria magdalena <neumariamagdalena@gmail.com>
2024-03-14 20:09:14 +0100

    make div-size dynamic by prompting user to insert size of grid

--- sketch.js
+++ sketch.js
@@ -3,18 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const numColumns = 16; 
     const numRows = 16;
 
+    do {
+        customGrid = parseInt(prompt("Enter a number between 0 and 100:"));
+    } while (customGrid < 0 || customGrid > 100);
+    
 
     
-    for (let i = 0; i < numColumns; i++) {
+    for (let i = 0; i < customGrid; i++) {
         const column = document.createElement("div");
         column.classList.add("column");
         myGrid.appendChild(column);
-       		for (let j = 0; j< numRows; j++) {
+       		for (let j = 0; j< customGrid; j++) {
        		const cell = document.createElement("div");
        		cell.classList.add("cell");
             cell.classList.add("hover");
 
-					cell.textContent = j + '/' + i;
 					column.appendChild(cell);
     	}    
     }
