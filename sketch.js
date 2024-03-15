52f50fe1272d9207da9b474323b1fc65abcb3168
maria magdalena <neumariamagdalena@gmail.com>
2024-03-15 16:05:51 +0100

    make every cell have own color (removed .hover part of code from inside the eventlistener, made randomBgColor return bgColor and called randomBgColor as value for element.style.backgroundColor

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const button = document.createElement("button");
     document.body.appendChild(button)
     document.body.prepend(button);
@@ -10,35 +10,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     button.addEventListener("click", () => {
-    customGrid = parseInt(prompt("insert a number between 0 & 100"));
-    for (let i = 0; i < customGrid; i++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        myGrid.appendChild(column);
-       		for (let j = 0; j< customGrid; j++) {
-       		const cell = document.createElement("div");
-       		cell.classList.add("cell");
-            cell.classList.add("hover");
+        customGrid = parseInt(prompt("insert a number between 0 & 100"));
+        for (let i = 0; i < customGrid; i++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            myGrid.appendChild(column);
+            for (let j = 0; j < customGrid; j++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.classList.add("hover");
+                column.appendChild(cell);
+            }
+        }
 
-					column.appendChild(cell);
-    	}    
-    }
+        function randomBgColor() {
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+            var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+            console.log(bgColor);
+            return bgColor;
+        }
 
-    function randomBgColor() {
-        var x = Math.floor(Math.random() * 256);
-        var y = Math.floor(Math.random() * 256);
-        var z = Math.floor(Math.random() * 256);
-        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        console.log(bgColor);
         const hover = document.querySelectorAll(".hover");
-    hover.forEach((element) => {
-   		element.addEventListener("mouseover", (event) => {
-        element.style.backgroundColor = bgColor;
+        hover.forEach((element) => {
+            element.addEventListener("mouseover", (event) => {
+                element.style.backgroundColor = randomBgColor();
+            });
         });
-    });
-    }
-    
-    randomBgColor();
 
-});
+    });
+
 });
