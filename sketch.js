93d59fc7ce6e44d3ae30ce2ad9ffb42046cf32ba
maria magdalena <neumariamagdalena@gmail.com>
2024-03-16 19:55:57 +0100

    make custom grid after clearing previous one using myGrid.innerHTML=;

--- sketch.js
+++ sketch.js
@@ -3,26 +3,60 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(button)
     document.body.prepend(button);
 
-    button.textContent = "press me to set grid size";
+    button.textContent = "press me to set a different grid size";
     const myGrid = document.querySelector("#myGrid")
-    let customGrid = "";
+    let startGrid = 16;
+    let newGrid = "";
 
 
-
-    button.addEventListener("click", () => {
-        customGrid = parseInt(prompt("insert a number between 0 & 100"));
-        for (let i = 0; i < customGrid; i++) {
+    function createStartGrid() {
+        for (let i = 0; i < startGrid; i++) {
             const column = document.createElement("div");
             column.classList.add("column");
             myGrid.appendChild(column);
-            for (let j = 0; j < customGrid; j++) {
+            for (let j = 0; j < startGrid; j++) {
                 const cell = document.createElement("div");
                 cell.classList.add("cell");
                 cell.classList.add("hover");
                 column.appendChild(cell);
             }
+            function randomBgColor() {
+                var x = Math.floor(Math.random() * 256);
+                var y = Math.floor(Math.random() * 256);
+                var z = Math.floor(Math.random() * 256);
+                var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+                console.log(bgColor);
+                return bgColor;
+            }
+    
+            const hover = document.querySelectorAll(".hover");
+            hover.forEach((element) => {
+                element.addEventListener("mouseover", (event) => {
+                    element.style.backgroundColor = randomBgColor();
+                });
+            });
+    
         }
+    }
+    
+    createStartGrid();
 
+    button.addEventListener("click", () => {
+        customGrid = parseInt(prompt("insert a number between 0 & 100"));
+        myGrid.innerHTML="";
+        for (let i = 0; i < customGrid; i++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            myGrid.appendChild(column);
+                   for (let j = 0; j< customGrid; j++) {
+                   const cell = document.createElement("div");
+                   cell.classList.add("cell");
+                cell.classList.add("hover");
+    
+                        column.appendChild(cell);
+            }    
+        }
+        
         function randomBgColor() {
             var x = Math.floor(Math.random() * 256);
             var y = Math.floor(Math.random() * 256);
